305238c248ca4fc786114d6c1c5162dd73132e69
marco-pontes <aurelio.pontes@gmail.com>
2025-10-22 21:54:49 -0300

    Adding new icons and adding backend api

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -12,7 +12,6 @@ export const Contact = (): FunctionComponent => {
 	const onValidFn = (data: FieldValues): void => {
 		fetch("api/contact", {
 			method: "POST",
-
 			body: JSON.stringify(data),
 			headers: {
 				Accept: "application/json",

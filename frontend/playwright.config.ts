a9c8efea07f38d9ff433dd82d380e9016bd4664e
marco-pontes <aurelio.pontes@gmail.com>
2025-10-22 23:14:41 -0300

    Changing cloud build config

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -23,8 +25,8 @@ export default defineConfig({
 	reporter: "html",
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
-		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: "http://127.0.0.1:5173",
+		/* Base URL to use in actions like `await page.goto('')`. */
+		// baseURL: 'http://localhost:3000',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
@@ -69,10 +71,9 @@ export default defineConfig({
 	],
 
 	/* Run your local dev server before starting the tests */
-	webServer: {
-		command: "npm -s start",
-		url: "http://127.0.0.1:5173",
-		reuseExistingServer: !process.env["CI"],
-		timeout: 120000,
-	},
+	// webServer: {
+	//   command: 'npm run start',
+	//   url: 'http://localhost:3000',
+	//   reuseExistingServer: !process.env.CI,
+	// },
 });
